9b01bb59b0b092b7477ec73932cfa31263096f87
yuki_kashiwagi <kashiwa71@gmail.com>
2023-11-03 23:50:52 +0900

    gptの実装

--- src/app/prompt/prompt.component.ts
+++ src/app/prompt/prompt.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import OpenAI from 'openai';
 
 @Component({
   selector: 'app-prompt',
@@ -9,7 +10,7 @@ import { Component } from '@angular/core';
 export class PromptComponent {
   prompt = '';
   enterdPrompt = '';
-
+  response = '';
 
   constructor( private http: HttpClient) { }
   
@@ -21,5 +22,22 @@ export class PromptComponent {
     });
   }
 
-
+  onAddPost(){
+    const encodeUrlString = encodeURIComponent(this.enterdPrompt)
+    const url = "http://localhost:3000/imgen/" + encodeUrlString
+    console.log(url)
+    this.http.post(url, {})
+    .subscribe(response => {
+      console.log(response);
+    })
+  }
+  
+  callGPT(){
+    // post submitted prompt to server and get response
+    this.http.post<{role:string, content:string}>('http://localhost:3000/gpt', {prompt: this.enterdPrompt})
+    .subscribe(response => {
+      console.log(response);
+      this.response = response.content;
+    })
+  }
 }
